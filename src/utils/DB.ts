08404e129cb4b1e54a28854ee534d8a6b0bc4d39
PTimurV <stud0000222598@study.utmn.ru>
2022-07-21 15:33:03 +0500

    adaptive7

---
+++ src/utils/DB.ts
@@ -0,0 +1,18 @@
+// export default class dataBase {
+//     Card1={
+//         "id": 2345467,
+//         "describtion": "Линолеум Бытовой, Juteks, коллекция Avanta,«Fresco 5_FR22».... ",
+//
+//     }
+//
+//
+//
+//
+// }
+
+
+
+
+
+
+
